949ba569794e48081bbd90ad9692fa83fe4308a9
taisaoko <ttd3calvin@gmail.com>
2018-08-28 15:31:01 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,7 @@ class Shoe
   BRANDS =[]
   def initialize(brand)
     @brand = brand
-    BRANDS << brand if BRANDS.include?(brand)
+    BRANDS << brand if BRANDS.include?(brand) == false
   end
 
   def cobble
